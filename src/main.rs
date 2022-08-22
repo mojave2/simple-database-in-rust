080c4abd28aa35fa3287881b74b700590b9259a0
mojave2 <chenchen145@huawei.com>
2022-08-22 11:02:44 +0800

    part2. world's simplest SQL compiler and VM

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,70 @@ fn main() -> io::Result<()> {
         io::stdin().read_line(&mut buffer)?;
         let command = buffer.trim();
 
-        if command == ".exit" {
-            std::process::exit(0);
-        } else {
-            println!("Unrecognized command: {:?}.", command);
+        if command.chars().next().unwrap() == '.' {
+            match do_meta_command(command) {
+                Ok(_) => continue,
+                Err(e) => {
+                    println!("{}", e);
+                    continue;
+                }
+            }
+        }
+
+        match prepare_statement(command) {
+            Ok(statement) => {
+                execute_statement(statement);
+                println!("Executed.");
+            }
+            Err(e) => {
+                println!("{}", e);
+                continue;
+            }
         }
     }
 }
 
 fn print_prompt() {
     print!("db> ");
-    //let mut stdout = io::stdout();
-    //write!(stdout, "db> ");
     io::stdout().flush().unwrap();
 }
 
+
+fn do_meta_command(command: &str) -> Result<(), String> {
+    if command == ".exit" {
+        std::process::exit(0);
+    } else {
+        Err(format!("Unrecognized Meta Command: {:?}", command))
+    }
+}
+
+enum StatementType {
+    Insert,
+    Select,
+}
+
+struct Statement {
+    type_: StatementType,
+}
+
+fn prepare_statement(command: &str) -> Result<Statement, String> {
+    if &command[..6] == "insert" {
+        Ok(Statement { type_: StatementType::Insert })
+    } else if &command[..6] == "select" {
+        Ok(Statement { type_: StatementType::Select })
+    } else {
+        Err(format!("Unrecognized Statement: {:?}", command))
+    }
+}
+
+fn execute_statement(statement: Statement) {
+    use StatementType::*;
+    match statement.type_ {
+        Insert => {
+            println!("This is where we would do an insert.");
+        }
+        Select => {
+            println!("This is where we would do a select.");
+        }
+    }
+}
