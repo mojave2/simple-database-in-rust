4679ee261a9dc7b6705689a951e9c07f564b6f80
mojave2 <chenchen145@huawei.com>
2022-08-23 16:03:57 +0800

    part5. persistence to disk

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
-use bytes::{BufMut, BytesMut};
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 use serde::{Deserialize, Serialize};
-use std::io::{self, Write};
+use std::{
+    fs::{File, OpenOptions},
+    io::{self, BufRead, Cursor, Read, Seek, SeekFrom, Write},
+    path::Path,
+};
 
 fn main() -> io::Result<()> {
-    let mut table = Table::new();
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Must supply a database filename.");
+        std::process::exit(-1);
+    }
+    let filename = &args[1];
+    let mut table = Table::open_db(filename);
     let mut buffer = String::new();
     loop {
         buffer.clear();
@@ -12,7 +22,7 @@ fn main() -> io::Result<()> {
         let command = buffer.trim();
 
         if command.starts_with('.') {
-            match do_meta_command(command) {
+            match do_meta_command(command, &mut table) {
                 Ok(_) => continue,
                 Err(e) => {
                     println!("{}", e);
@@ -42,8 +52,9 @@ fn print_prompt() {
     io::stdout().flush().unwrap();
 }
 
-fn do_meta_command(command: &str) -> Result<(), String> {
+fn do_meta_command(command: &str, table: &mut Table) -> Result<(), String> {
     if command == ".exit" {
+        table.close_db()?;
         std::process::exit(0);
     } else {
         Err(format!("Unrecognized Meta Command: {:?}", command))
@@ -109,22 +120,20 @@ fn execute_insert(stmt: Statement, table: &mut Table) -> Result<(), String> {
         return Err("Execute Table Full".to_owned());
     }
     let row = table.row_slot(table.num_rows);
-    if row.is_none() {
-        *row = Some(BytesMut::with_capacity(ROW_SIZE));
-    }
-    stmt.row_to_insert.unwrap().serialize(row.as_mut().unwrap());
+    stmt.row_to_insert.unwrap().serialize(row);
     table.num_rows += 1;
     Ok(())
 }
 
 fn execute_select(_stmt: Statement, table: &mut Table) -> Result<(), String> {
     for i in 0..table.num_rows {
-        let row = Row::deserialize(table.row_slot(i).as_mut().unwrap());
+        let row = Row::deserialize(table.row_slot(i));
         println!("{:?}", row);
     }
     Ok(())
 }
 
+const COLUMN_ID_SIZE: usize = std::mem::size_of::<u32>();
 const COLUMN_USERNAME_SIZE: usize = 32;
 const COLUMN_EMAIL_SIZE: usize = 255;
 
@@ -137,16 +146,16 @@ struct Row {
 
 impl Row {
     fn new(id: u32, username: &str, email: &str) -> Result<Self, String> {
-        if username.len() > COLUMN_USERNAME_SIZE {
+        if username.len() > COLUMN_USERNAME_SIZE - 1 {
             return Err(format!(
                 "username string is too long. (>{})",
-                COLUMN_USERNAME_SIZE
+                COLUMN_USERNAME_SIZE - 1
             ));
         }
-        if email.len() > COLUMN_EMAIL_SIZE {
+        if email.len() > COLUMN_EMAIL_SIZE - 1 {
             return Err(format!(
                 "email string is too long. (>{})",
-                COLUMN_EMAIL_SIZE
+                COLUMN_EMAIL_SIZE - 1
             ));
         }
         Ok(Row {
@@ -155,39 +164,160 @@ impl Row {
             email: email.to_owned(),
         })
     }
-    fn serialize(&self, destination: &mut BytesMut) {
-        let row_bin = bincode::serialize(&self).unwrap();
-        destination.put(&row_bin[..]);
+
+    fn serialize(&self, buf: &mut RowBuf) {
+        let mut cur = Cursor::new(buf);
+        cur.write_u32::<LittleEndian>(self.id).unwrap();
+        cur.seek(SeekFrom::Start(COLUMN_ID_SIZE as u64)).unwrap();
+        let _ = cur.write(self.username.as_bytes()).unwrap();
+        cur.seek(SeekFrom::Start(
+            (COLUMN_ID_SIZE + COLUMN_USERNAME_SIZE) as u64,
+        ))
+        .unwrap();
+        let _ = cur.write(self.email.as_bytes()).unwrap();
     }
-    fn deserialize(destination: &mut BytesMut) -> Self {
-        bincode::deserialize(destination).unwrap()
+
+    fn deserialize(buf: &mut RowBuf) -> Self {
+        let mut cur = Cursor::new(buf);
+        let id = cur.read_u32::<LittleEndian>().unwrap();
+        cur.seek(SeekFrom::Start(COLUMN_ID_SIZE as u64)).unwrap();
+        let mut username: Vec<u8> = Vec::new();
+        cur.read_until(0, &mut username).unwrap();
+        let username = &username[..username.len() - 1];
+        cur.seek(SeekFrom::Start(
+            (COLUMN_ID_SIZE + COLUMN_USERNAME_SIZE) as u64,
+        ))
+        .unwrap();
+        let mut email: Vec<u8> = Vec::new();
+        cur.read_until(0, &mut email).unwrap();
+        let email = &email[..email.len() - 1];
+        Self {
+            id,
+            username: String::from_utf8_lossy(username).into(),
+            email: String::from_utf8_lossy(email).into(),
+        }
     }
 }
 
-const ROW_SIZE: usize = 291;
+const ROW_SIZE: usize = std::mem::size_of::<u32>() + COLUMN_EMAIL_SIZE + COLUMN_USERNAME_SIZE;
 const PAGE_SIZE: usize = 4096;
 const TABLE_MAX_PAGES: usize = 100;
 const ROWS_PER_PAGE: usize = PAGE_SIZE / ROW_SIZE;
 const TABLE_MAX_ROWS: usize = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
+type RowBuf = [u8];
+type Page = [u8; PAGE_SIZE];
+
 struct Table {
     num_rows: usize,
-    pages: [[Option<BytesMut>; ROWS_PER_PAGE]; TABLE_MAX_PAGES],
+    pager: Pager,
 }
 
 impl Table {
-    fn new() -> Self {
-        const INIT_ROW: Option<BytesMut> = None;
-        const INIT_PAGE: [Option<BytesMut>; ROWS_PER_PAGE] = [INIT_ROW; ROWS_PER_PAGE];
-        Table {
-            num_rows: 0,
+    fn open_db<P>(fname: P) -> Self
+    where
+        P: AsRef<Path>,
+    {
+        let pager = Pager::open(fname);
+        let num_rows = pager.file_length / ROW_SIZE;
+        Table { num_rows, pager }
+    }
+
+    fn row_slot(&mut self, row_num: usize) -> &mut RowBuf {
+        let page_num = row_num / ROWS_PER_PAGE;
+        let page = self.pager.get_page(page_num);
+        let row_offset = row_num % ROWS_PER_PAGE;
+        let byte_offset = row_offset * ROW_SIZE;
+        &mut page[byte_offset..byte_offset + ROW_SIZE]
+    }
+
+    fn close_db(&mut self) -> Result<(), String> {
+        let pager = &mut self.pager;
+        let num_full_pages = self.num_rows / ROWS_PER_PAGE;
+        for i in 0..num_full_pages {
+            if pager.pages[i].is_some() {
+                pager.flush(i, PAGE_SIZE)?;
+                pager.pages[i] = None;
+            }
+        }
+
+        let num_additional_rows = self.num_rows % ROWS_PER_PAGE;
+        if num_additional_rows > 0 {
+            let page_num = num_full_pages;
+            if pager.pages[page_num].is_some() {
+                pager.flush(page_num, num_additional_rows * ROW_SIZE)?;
+                pager.pages[page_num] = None;
+            }
+        }
+
+        self.pager.close()?;
+        Ok(())
+    }
+}
+
+struct Pager {
+    file_handle: File,
+    file_length: usize,
+    pages: [Option<Page>; TABLE_MAX_PAGES],
+}
+
+impl Pager {
+    fn open<P>(fname: P) -> Self
+    where
+        P: AsRef<Path>,
+    {
+        let file_handle = OpenOptions::new()
+            .write(true)
+            .read(true)
+            .open(fname)
+            .expect("Unable to open file.");
+        let file_length = file_handle.metadata().unwrap().len() as usize;
+        const INIT_PAGE: Option<Page> = None;
+        Self {
+            file_handle,
+            file_length,
             pages: [INIT_PAGE; TABLE_MAX_PAGES],
         }
     }
 
-    fn row_slot(&mut self, row_num: usize) -> &mut Option<BytesMut> {
-        let page = &mut self.pages[row_num / ROWS_PER_PAGE];
-        &mut page[row_num % ROWS_PER_PAGE]
+    fn get_page(&mut self, page_num: usize) -> &mut Page {
+        if page_num > TABLE_MAX_PAGES {
+            eprintln!("Tried to fetch page number out of bounds. {}", page_num);
+            std::process::exit(-1);
+        }
+        if self.pages[page_num].is_none() {
+            self.pages[page_num] = Some([0u8; PAGE_SIZE]);
+            let mut num_pages = self.file_length / PAGE_SIZE;
+            // we might save a partial page at the end of the file
+            if self.file_length % PAGE_SIZE > 0 {
+                num_pages += 1;
+            }
+
+            if page_num <= num_pages {
+                self.file_handle
+                    .seek(SeekFrom::Start((page_num * PAGE_SIZE) as u64))
+                    .unwrap();
+                let _ = self
+                    .file_handle
+                    .read(self.pages[page_num].as_mut().unwrap())
+                    .unwrap();
+            }
+        }
+        self.pages[page_num].as_mut().unwrap()
+    }
+
+    fn flush(&mut self, page_num: usize, page_size: usize) -> Result<(), String> {
+        self.file_handle
+            .seek(SeekFrom::Start((page_num * PAGE_SIZE) as u64))
+            .expect("Error seeking offset");
+        self.file_handle
+            .write(&self.pages[page_num].unwrap()[..page_size])
+            .map_err(|_| "Error writing page buffer".to_owned())?;
+        Ok(())
+    }
+
+    fn close(&mut self) -> Result<(), String> {
+        Ok(())
     }
 }
 
